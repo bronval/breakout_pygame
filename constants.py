9be4396fe25bd1a376cf3d0ae6753a8b0a065289
bronval <benoit.ronval@student.uclouvain.be>
2021-07-15 18:32:43 +0200

    comments

---
+++ constants.py
@@ -0,0 +1,38 @@
+
+#####################################################
+#
+# breakout game with pygame
+# for Python stage in Technofutur TIC (Belgium)
+
+# Authors : Bastien Wiaux and Benoit Ronval
+#
+#####################################################
+
+
+# Constants for screen
+
+SCREEN_WIDTH = 
+SCREEN_HEIGHT =
+SCREEN_MARGIN =
+
+
+# constants for player
+
+PLAYER_WIDTH =
+PLAYER_HEIGHT = 
+PLAYER_SPEED = 
+
+
+# constants for ball
+
+BALL_RADIUS = 
+
+
+# constants for bricks
+
+BRICK_WIDTH = 
+BRICK_HEIGHT =
+BRICK_ROWS =
+BRICK_COLUMNS =
+BRICK_GAP =
+
---
+++ main.py
@@ -0,0 +1,9 @@
+
+#####################################################
+#
+# breakout game with pygame
+# for Python stage in Technofutur TIC (Belgium)
+
+# Authors : Bastien Wiaux and Benoit Ronval
+#
+#####################################################
